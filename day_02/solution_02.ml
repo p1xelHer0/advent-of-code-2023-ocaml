35d1d66181db633988f0afaf9737a449ca4d7937
Pontus Nagy <p_nagy@icloud.com>
2023-12-03 00:09:05 +0100

    parse god x good night

--- day_02/solution_02.ml
+++ day_02/solution_02.ml
@@ -13,177 +13,86 @@ module Game = struct
     type t = { red : int; green : int; blue : int }
     [@@deriving show { with_path = false }, make, eq]
 
-    let set_red x t = { t with red = x }
-    let set_green x t = { t with green = x }
-    let set_blue x t = { t with blue = x }
-    let test = Test.testable pp equal
-
-    let smoosh =
-      List.fold_left
-        ~f:(fun s1 s2 ->
-          let red = s1.red + s2.red in
-          let green = s1.green + s2.green in
-          let blue = s1.blue + s2.blue in
-          make ~red ~green ~blue)
-        ~init:(make ~red:0 ~green:0 ~blue:0)
-
-    let%test "set_color" =
-      let set = make ~red:1 ~green:2 ~blue:3 in
-
-      Test.(
-        run test (set |> set_red 10) ~expect:(make ~red:10 ~green:2 ~blue:3));
-      Test.(
-        run test (set |> set_green 10) ~expect:(make ~red:1 ~green:10 ~blue:3));
-      Test.(
-        run test (set |> set_blue 10) ~expect:(make ~red:1 ~green:2 ~blue:10))
+    let empty = make ~red:0 ~green:0 ~blue:0
+    let incr_red i t = { t with red = t.red + i }
+    let incr_green i t = { t with green = t.green + i }
+    let incr_blue i t = { t with blue = t.blue + i }
   end
 
   type t = { id : int; rounds : Round.t list }
-  [@@deriving show { with_path = false }, eq, make]
-
-  let test = Test.testable pp equal
-
-  let count_red t =
-    List.fold_left ~f:Round.(fun a t -> a + t.red) ~init:0 t.rounds
-
-  let count_green t =
-    List.fold_left ~f:Round.(fun a t -> a + t.green) ~init:0 t.rounds
-
-  let count_blue t =
-    List.fold_left ~f:Round.(fun a t -> a + t.blue) ~init:0 t.rounds
-
-  let%test "count_color" =
-    let game : t =
-      make ~id:1
-        ~rounds:
-          [
-            Set.make ~red:1 ~green:1 ~blue:1;
-            Set.make ~red:2 ~green:1 ~blue:1;
-            Set.make ~red:3 ~green:1 ~blue:1;
-            Set.make ~red:4 ~green:0 ~blue:1;
-            Set.make ~red:3 ~green:0 ~blue:3;
-          ]
-        ()
-    in
-
-    Test.(run Test.int (game |> count_red) ~expect:13);
-    Test.(run Test.int (game |> count_green) ~expect:3);
-    Test.(run Test.int (game |> count_blue) ~expect:7)
 end
 
 module P = struct
   open Angstrom
 
-  let is_whitespace = function ' ' | '\t' -> true | _ -> false
   let is_digit = function '0' .. '9' -> true | _ -> false
-  let is_char = function 'a' .. 'z' -> true | _ -> false
-  let whitespace = take_while is_whitespace
-  let number = take_while1 is_digit >>| Int.of_string_exn
-  let game_id = string "Game " *> number <* string ": "
+  let int = take_while1 is_digit >>| Int.of_string_exn
+  let newline = string "\n"
+  let comma = string ", "
+  let semicolon = string "; "
+  let game_id = string "Game " *> int <* string ": "
+  let red = int <* string " red" >>| fun n -> `Red n
+  let green = int <* string " green" >>| fun n -> `Green n
+  let blue = int <* string " blue" >>| fun n -> `Blue n
+  let possible_colors = red <|> green <|> blue
 
-  let%test "parse game_id" =
-    Test.(
-      run
-        (Test.result Test.int Test.string)
-        (parse_string ~consume:All game_id "Game 13: ")
-        ~expect:(Ok 13))
+  let round =
+    sep_by comma possible_colors
+    >>| Game.Round.(
+          List.fold_left
+            ~f:(fun x y ->
+              match y with
+              | `Red i -> incr_red i x
+              | `Green i -> incr_green i x
+              | `Blue i -> incr_blue i x)
+            ~init:empty)
 
-  let red =
-    number <* string " red" >>| fun n ->
-    Game.Set.{ red = n; green = 0; blue = 0 }
+  let all_rounds = sep_by semicolon round
 
-  let%test "parse red" =
-    Test.(
-      run
-        (Test.result Game.Set.test Test.string)
-        (parse_string ~consume:All red "4 red")
-        ~expect:(Ok (Game.Set.make ~red:4 ~green:0 ~blue:0)))
+  let game =
+    sep_by newline
+      ( game_id >>= fun id ->
+        all_rounds >>| fun rounds -> Game.{ id; rounds } )
 
-  let green =
-    number <* string " green" >>| fun n ->
-    Game.Set.{ red = 0; green = n; blue = 0 }
-
-  let%test "parse green" =
-    Test.(
-      run
-        (Test.result Game.Set.test Test.string)
-        (parse_string ~consume:All green "5 green")
-        ~expect:(Ok (Game.Set.make ~red:0 ~green:5 ~blue:0)))
-
-  let blue =
-    number <* string " blue" >>| fun n ->
-    Game.Set.{ red = 0; green = 0; blue = n }
-
-  let%test "parse blue" =
-    Test.(
-      run
-        (Test.result Game.Set.test Test.string)
-        (parse_string ~consume:All blue "3 blue")
-        ~expect:(Ok (Game.Set.make ~red:0 ~green:0 ~blue:3)))
-
-  let cube = red <|> green <|> blue
-  let sep = string ", "
-  let end_ = string "; "
-  let game_set_smooshed = sep_by sep cube >>| Game.Set.smoosh
-
-  let%test "parse game_set_smooshed" =
-    let text = "5 red, 10 blue, 20 green, 5 red, 10 blue, 10 blue" in
-    Test.(
-      run
-        (Test.result Game.Set.test Test.string)
-        (parse_string ~consume:All game_set_smooshed text)
-        ~expect:(Ok (Game.Set.make ~red:10 ~green:20 ~blue:30)))
-
-  let game_smooshed = sep_by end_ game_set_smooshed
-
-  let%test "parse game" =
-    let text =
-      "5 red, 10 blue, 20 green, 5 red, 10 blue, 10 blue; 1 red, 1 green, 1 blue, 1 red; 1 blue"
-    in
-    Test.(
-      run
-        (Test.result (Test.list Game.Set.test) Test.string)
-        (parse_string ~consume:All game_smooshed text)
-        ~expect:
-          (Ok
-             [
-               Game.Set.make ~red:10 ~green:20 ~blue:30;
-               Game.Set.make ~red:2 ~green:1 ~blue:1;
-               Game.Set.make ~red:0 ~green:0 ~blue:1;
-             ]))
-
-  let parse_a input =
-    parse_string ~consume:All game_smooshed input |> Result.get_or_failwith
-
-  let parsers =
-    Util.
-      [
-        parse "Game %i: %s" (fun game_id input ->
-            Game.{ id = game_id; rounds = parse_a input });
-      ]
-
-  let parse_instructions = Util.try_parse parsers
+  let parse input =
+    parse_string ~consume:All game input |> Result.get_or_failwith
 end
 
 module Part_1 = struct
   let solve input =
-    let lines = String.lines input in
-    let parsed = List.map ~f:P.parse_instructions lines in
-    List.iter ~f:(fun g -> print_endline (Game.show g)) parsed;
-    0
+    let open Game in
+    let games = P.parse input in
+    let possible game =
+      List.for_all
+        ~f:(fun Round.{ red; green; blue } ->
+          red <= 12 && green <= 13 && blue <= 14)
+        game.rounds
+    in
+    let possible_games = List.filter ~f:possible games in
+    List.fold_left ~f:(fun i g -> i + g.id) ~init:0 possible_games
 
   let%test "sample data" = Test.(run int (solve sample) ~expect:8)
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let open Game in
+    let open Round in
+    let games = P.parse input in
+    let max_gems =
+      List.fold_left
+        ~f:(fun r1 r2 ->
+          make ~red:(max r1.red r2.red) ~green:(max r1.green r2.green)
+            ~blue:(max r1.blue r2.blue))
+        ~init:empty
+    in
+    games
+    |> List.map ~f:(fun g ->
+           max_gems g.rounds |> fun g -> g.red * g.green * g.blue)
+    |> Util.sum
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:2286)
 end
 
-let run_1 () =
-  (* Run.solve_int (module Part_1); *)
-  ()
-
-let run_2 () =
-  (* Run.solve_int (module Part_2); *)
-  ()
+let run_1 () = Run.solve_int (module Part_1)
+let run_2 () = Run.solve_int (module Part_2)
