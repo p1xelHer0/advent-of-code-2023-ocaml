14db7a298ce1605ea8207e07e56cd58c89d70c9e
Pontus Nagy <p_nagy@icloud.com>
2023-12-04 08:03:00 +0100

    day 4 wip lets go2work

--- day_04/solution_04.ml
+++ day_04/solution_04.ml
@@ -1,15 +1,31 @@
-let sample = {|
-...
-|} |> String.trim
+let sample =
+  {|
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+|}
+  |> String.trim
+
+module P = struct
+  open Angstrom
+
+  let is_digit = function '0' .. '9' -> true | _ -> false
+  let int = take_while1 is_digit >>| Int.of_string_exn
+  let rounds = sep_by (string "\n") (string "Card " *> int <* string ": ")
+  let parse input = parse_string ~consume:All _ input |> Result.get_or_failwith
+end
 
 module Part_1 = struct
   let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let%test "sample data" = Test.(run int (solve sample) ~expect:13)
 end
 
 module Part_2 = struct
   let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  (* let%test "sample data" = Test.(run int (solve sample) ~expect:0) *)
 end
 
 let run_1 () =
